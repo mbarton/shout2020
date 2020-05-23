1b2faad5dbc755c15f84cb6430bac8441d2fce1b
Michael Barton <michael@mbarton.cc>
2020-05-23 13:50:25 +0100

    WIP start with peer js

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Peer from 'peerjs';
+
 function App() {
+  const [id, setId] = useState<string | undefined>(undefined);
+  
+  useEffect(() => {
+    const peer = new Peer();
+    peer.on('open', setId);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {id}
       </header>
     </div>
   );
