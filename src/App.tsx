fe25400d6eb1f43bac9b4cc30f9e79eb3764f6ae
Michael Barton <michael@mbarton.cc>
2020-05-23 14:13:08 +0100

    Run peer locally and make some connections!

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,47 @@ import './App.css';
 
 import Peer from 'peerjs';
 
+const peer = new Peer({
+  host: "penguin.linux.test",
+  port: 9000
+});
+
 function App() {
   const [id, setId] = useState<string | undefined>(undefined);
+  const [remoteId, setRemoteId] = useState<string | undefined>(undefined);
   
   useEffect(() => {
-    const peer = new Peer();
     peer.on('open', setId);
+
+    peer.on('connection', conn => {
+      console.log('Connecion from ' + conn.peer);
+    });
   }, []);
+
+  function connect() {
+    if(remoteId) {
+      const conn = peer.connect(remoteId);
+
+      conn.on('open', () => {
+        console.log("Connected to " + remoteId);
+      });
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         {id}
+        <input
+          type="text"
+          placeholder="enter remote ID"
+          value={remoteId || ''}
+          onChange={(e) => setRemoteId(e.target.value)}
+        />
+        <button
+          disabled={remoteId === undefined || remoteId.length === 0}
+          onClick={connect}
+        >Connect</button>
       </header>
     </div>
   );
